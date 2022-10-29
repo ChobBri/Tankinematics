98d24d18170308a0a5f8e5b3efd98f895d51214a
Brian Cho <b.cho@queensu.ca>
2022-10-29 17:03:32 -0400

    Create Level struct

---
+++ Level.h
@@ -0,0 +1,25 @@
+#ifndef LEVEL_H
+#define LEVEL_H
+
+#include "raylib.h"
+#include <string>
+#include <vector>
+
+/**
+ * Level represents the context of a level.
+ * 
+ * It contains the minimum amount of data to represent a level.
+*/
+struct Level 
+{
+    float gravity = 0.0f;
+    float angle = 0.0f;
+    Vector2 initVelocity = {0.0f, 0.0f};
+    Vector2 tankPosition = {0.0f, 0.0f};
+    Vector2 targetPosition = {0.0f, 0.0f};
+    std::vector<std::string> hints;  // Text to provide hint to user. There can be multiple hints
+    std::string solution;  // Text to provide solution to user
+    bool angleOverVel = true;  // Flag to determine whether to solve for angle or velocity
+};
+
+#endif
