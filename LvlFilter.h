ab5fe8fab25cf64203124d1f99356aa9addf6fec
tylersun2 <47325178+tylersun2@users.noreply.github.com>
2022-11-20 22:15:24 -0500

    level filter gui

--- LvlFilter.h
+++ LvlFilter.h
@@ -1,9 +1,16 @@
-class LvlFilter{
-        public:        
-        LvlFilter();
-        void updateArray(bool isGrav, bool isAngle, bool isVelX, bool isVelY, bool isSpeed);
+#ifndef LVLFILTER_H
+#define LVLFILTER_H
 
+// Array to store settings to generate specific levels
+class LvlFilter{
+public:        
+        LvlFilter();
+        LvlFilter(bool, bool, bool, bool, bool);
+        void updateArray(bool isGrav, bool isAngle, bool isVelX, bool isVelY, bool isSpeed);
+        void toggleIndex(int);
         bool* getFilters();
-        private:
+private:
         bool filters[5];
 };
+
+#endif
